4edaeaf7ebe0eb956ddbb2fd8ef7967e3f05a0b3
Jenna-yuan <61236210+Jenna-yuan@users.noreply.github.com>
2020-03-27 15:08:35 +0800

    Add files via upload

---
+++ src/com/nwnu/pojo/Teacher.java
@@ -0,0 +1,161 @@
+package com.nwnu.pojo;
+
+import java.util.Date;
+
+public class Teacher {
+	private Integer t_id;
+	private String t_work_place;
+	private Date t_time;
+	private String t_name;
+	private String t_code;
+	private String t_number;
+	private String t_province;
+	private String t_city;
+	private String t_area;
+	private Integer am;
+	private Integer bm;
+	private Integer cm;
+	private Integer dm;
+	private Integer em;
+	private Integer fm;
+	private Integer gm;
+
+	public Integer getT_id() {
+		return t_id;
+	}
+
+	public void setT_id(Integer t_id) {
+		this.t_id = t_id;
+	}
+
+	public String getT_work_place() {
+		return t_work_place;
+	}
+
+	public void setT_work_place(String t_work_place) {
+		this.t_work_place = t_work_place;
+	}
+
+	public Date getT_time() {
+		return t_time;
+	}
+
+	public void setT_time(Date t_time) {
+		this.t_time = t_time;
+	}
+
+	public String getT_name() {
+		return t_name;
+	}
+
+	public void setT_name(String t_name) {
+		this.t_name = t_name;
+	}
+
+	public String getT_code() {
+		return t_code;
+	}
+
+	public void setT_code(String t_code) {
+		this.t_code = t_code;
+	}
+
+	public String getT_number() {
+		return t_number;
+	}
+
+	public void setT_number(String t_number) {
+		this.t_number = t_number;
+	}
+
+	public String getT_province() {
+		return t_province;
+	}
+
+	public void setT_province(String t_province) {
+		this.t_province = t_province;
+	}
+
+	public String getT_city() {
+		return t_city;
+	}
+
+	public void setT_city(String t_city) {
+		this.t_city = t_city;
+	}
+
+	public String getT_area() {
+		return t_area;
+	}
+
+	public void setT_area(String t_area) {
+		this.t_area = t_area;
+	}
+
+	public Integer getAm() {
+		return am;
+	}
+
+	public void setAm(Integer am) {
+		this.am = am;
+	}
+
+	public Integer getBm() {
+		return bm;
+	}
+
+	public void setBm(Integer bm) {
+		this.bm = bm;
+	}
+
+	public Integer getCm() {
+		return cm;
+	}
+
+	public void setCm(Integer cm) {
+		this.cm = cm;
+	}
+
+	public Integer getDm() {
+		return dm;
+	}
+
+	public void setDm(Integer dm) {
+		this.dm = dm;
+	}
+
+	public Integer getEm() {
+		return em;
+	}
+
+	public void setEm(Integer em) {
+		this.em = em;
+	}
+
+	public Integer getFm() {
+		return fm;
+	}
+
+	public void setFm(Integer fm) {
+		this.fm = fm;
+	}
+
+	public Integer getGm() {
+		return gm;
+	}
+
+	public void setGm(Integer gm) {
+		this.gm = gm;
+	}
+
+	public void setStart(int i) {
+		// TODO Auto-generated method stub
+
+	}
+
+	public void setRows(Integer rows) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
